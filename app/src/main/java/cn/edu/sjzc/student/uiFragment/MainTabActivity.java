93dde62b850dbb63de87ea0e5f266e3fc51581cb
樊亚风 <1181631922@qq.com>
2015-06-01 10:10:07 +0800

    2015/05/31，基本细化完成

--- app/src/main/java/cn/edu/sjzc/student/uiFragment/MainTabActivity.java
+++ app/src/main/java/cn/edu/sjzc/student/uiFragment/MainTabActivity.java
@@ -34,7 +34,7 @@ public class MainTabActivity extends FragmentActivity implements View.OnClickLis
     private int mImageViewArray[] = {R.drawable.tab_home_btn, R.drawable.tab_message_btn, R.drawable.tab_selfinfo_btn,
             R.drawable.tab_square_btn, R.drawable.tab_more_btn};
 
-    private String mTextviewArray[] = {"首页", "查看评估", "评价老师", "查找教师", "个人信息"};
+    private String mTextviewArray[] = {"首页", "查看信息", "评价老师", "查找教师", "个人中心"};
     public static MainTabActivity instance = null;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
